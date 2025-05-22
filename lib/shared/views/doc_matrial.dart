f618197ecd8a4eb7c276215f9fa7d2ebbbdd888e
elsaramji <mahmoudelsaramegy@gmail.com>
2025-05-22 03:36:50 +0300

    -edit splash to android12 -add Splash , screenutil , gorouter,flutter svg , flutter icon luncher -eidit android ndk version -add figma design -add font to asset -add app martrial in shared file -genrate image paths -create app routes file -imp go_router -genrate app colors from design -genrate app text_style from design -handel dimensions  on context -build on_bordering_view ✅

--- lib/shared/views/doc_matrial.dart
+++ lib/shared/views/doc_matrial.dart
@@ -1,10 +1,21 @@
+import 'package:doctors_doc/config/routes/go_routs.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class DocApp extends StatelessWidget {
   const DocApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Placeholder());
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder:
+          (context, child) => MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            routerConfig: router,
+          ),
+    );
   }
 }
