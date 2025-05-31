96eab483ff39d11d6fa4ce863faa99d8b63fc91c
elsaramji <mahmoudelsaramegy@gmail.com>
2025-05-31 06:40:19 +0300

    imp regsirer api , error model , respons , user form and dio factory

---
+++ lib/futures/auth/data/model/user_form_model.dart
@@ -0,0 +1,23 @@
+import 'package:doctors_doc/futures/auth/domain/enitties/user_form_enitty.dart';
+
+class UserFormModel implements UserFormEnitty {
+  String email;
+  String password;
+  String name;
+  String phone;
+  UserFormModel({
+    required this.email,
+    required this.password,
+    required this.name,
+    required this.phone,
+  });
+  @override
+  Map<String, dynamic> toJson() => {
+    'email': email,
+    'password': password,
+    "gender": 0,
+    "password_confirmation": password,
+    'name': name,
+    'phone': phone,
+  };
+}
--- lib/futures/auth/presentation/widgets/craate_account_view.dart
+++ lib/futures/auth/presentation/widgets/craate_account_view.dart
@@ -1,62 +1,58 @@
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:doctors_doc/core/context/dimentions.dart';
-import 'package:doctors_doc/futures/auth/presentation/regsiter_cubit/regsiter_cubit.dart';
 import 'package:doctors_doc/futures/auth/presentation/widgets/create_account_form.dart';
 import 'package:doctors_doc/futures/auth/presentation/widgets/signup_header.dart';
 import 'package:doctors_doc/shared/presentation/widgets/custom_primary_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CreateAccountbyEmailView extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController phoneController = TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final Dio dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://vcare.integration25.com/api',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    ),
+  );
+
   CreateAccountbyEmailView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => RegsiterCubit(),
-      child: Builder(
-        builder: (context) {
-          return BlocBuilder<RegsiterCubit, RegsiterState>(
-            builder: (context, state) {
-              log("Current state: $state");
-
-              return SingleChildScrollView(
-                child: Column(
-                  children: [
-                    // Vertical space at the top
-                    context.verticalSpace(50),
-                    // Create account header widget
-                    CreateAccountTextHeader(),
-                    // Vertical space after the header
-                    context.verticalSpace(17),
-                    //
-                    CreateAccountForm(
-                      emailController: emailController,
-                      passwordController: passwordController,
-                      phoneController: phoneController,
-                    ),
-                    context.verticalSpace(32),
-                    PrimaryButton(
-                      text: "Sign Up",
-                      onPressed: () {
-                        // Call the register method from the RegsiterCubit
-                        context.read<RegsiterCubit>().register(
-                          emailController.text,
-                          passwordController.text,
-                          phoneController.text,
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              );
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Vertical space at the top
+          context.verticalSpace(50),
+          // Create account header widget
+          CreateAccountTextHeader(),
+          // Vertical space after the header
+          context.verticalSpace(17),
+          //
+          CreateAccountForm(
+            formKey: formKey,
+            emailController: emailController,
+            passwordController: passwordController,
+            phoneController: phoneController,
+          ),
+          context.verticalSpace(32),
+          PrimaryButton(
+            text: "Sign Up",
+            onPressed: () {
+              if (formKey.currentState?.validate() ?? false) {
+              } else {
+                log("Form validation failed");
+              }
             },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
