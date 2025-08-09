bd9857f8e712301b13813754b52b2f5de8e0b8b0
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-09 03:51:45 +0300

    demo implementation home view ui

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,13 +1,38 @@
 import 'package:doctors_doc/config/routes/constant_routs.dart';
+import 'package:doctors_doc/config/themes/colors.dart';
+import 'package:doctors_doc/features/home/presentation/widgets/home_app_bar.dart';
+import 'package:doctors_doc/features/home/presentation/widgets/home_banner_view.dart';
+import 'package:doctors_doc/features/home/presentation/widgets/home_bottom_app_bar.dart';
+import 'package:doctors_doc/features/home/presentation/widgets/home_doctors_list.dart';
+import 'package:doctors_doc/features/home/presentation/widgets/home_speciality_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeView extends StatelessWidget {
   static final routeName = ConstantRouts.home;
   const HomeView({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundwhite,
+      body: Container(
+        width: 1.sw,
+        height: 1.sh - 80.h,
+        padding: EdgeInsets.symmetric(horizontal: 16.w),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: HomeAppBar(userName: "John Doe")),
+            SliverToBoxAdapter(child: HomeBanner()),
+            SliverToBoxAdapter(child: HomeSpecialityList()),
+            // return SliverList to show list of doctors
+            HomeDoctorsList(),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.miniCenterDocked,
+      floatingActionButton: HomeBottomBar(),
+    );
   }
 }
