969aa8c33068c16cfa40a7bfa9fd1ccce6456260
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-09 03:41:39 +0300

    add home view routes

--- lib/config/routes/go_routs.dart
+++ lib/config/routes/go_routs.dart
@@ -8,7 +8,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
-  initialLocation: BordingView.route,
+  initialLocation: ConstantRouts.home,
   routes: [
     GoRoute(
       path: ConstantRouts.onbording,
