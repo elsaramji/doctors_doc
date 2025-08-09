036cbcd00bb6cb041aeba5878fb304d42fd206cb
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-09 03:42:48 +0300

    top app bar ui impl

---
+++ lib/features/home/presentation/widgets/home_app_bar.dart
@@ -0,0 +1,41 @@
+import 'package:doctors_doc/config/themes/colors.dart';
+import 'package:doctors_doc/config/themes/texts_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeAppBar extends StatelessWidget {
+  final String userName;
+  const HomeAppBar({super.key, required this.userName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 1.sw,
+      height: 72.h,
+      padding: EdgeInsets.symmetric(vertical: 16.h),
+      child: ListTile(
+        minVerticalPadding: 16.h,
+        title: Text(userName, style: AppTextStyles.interBold18()),
+        subtitle: Text(
+          "Welcome to DocDoc",
+          style: AppTextStyles.interRegular11().copyWith(
+            color: AppColors.text80,
+          ),
+        ),
+        trailing: Container(
+          width: 48.w,
+          height: 48.w,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: AppColors.text20,
+          ),
+          child: Icon(
+            Icons.notifications_none,
+            color: AppColors.text100,
+            size: 24.sp,
+          ),
+        ),
+      ),
+    );
+  }
+}
