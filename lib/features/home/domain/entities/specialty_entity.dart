2f605d6bbd4bca79ac5864541226383962bb4870
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-27 03:22:10 +0300

    create home entities

--- lib/features/home/domain/entities/specialty_entity.dart
+++ lib/features/home/domain/entities/specialty_entity.dart
@@ -1,5 +1,13 @@
+import 'package:doctors_doc/features/home/domain/entities/doctor_entity.dart';
+
 abstract class SpecialtyEntity {
+  final int id;
   final String name;
-  final String image;
-  SpecialtyEntity({required this.name, required this.image});
+  final List<DoctorEntity>? doctors;
+
+  SpecialtyEntity({
+    required this.name,
+    required this.id,
+    this.doctors,
+  });
 }
