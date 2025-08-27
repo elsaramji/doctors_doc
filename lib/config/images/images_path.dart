1b6c9a011c76dd6a209894e974b0622b72c3efba
Mahmoud Ahamed Elsaramji <90051301+elsaramji@users.noreply.github.com>
2025-08-27 03:19:13 +0300

    upload new assets

--- lib/config/images/images_path.dart
+++ lib/config/images/images_path.dart
@@ -6,15 +6,89 @@ class Assets {
   /// assets/images/app_icons.png
   static const String assetsImagesAppIcons = "assets/images/app_icons.png";
 
+  /// Assets for assetsImagesBackgroundHomeBordering
+  /// assets/images/Background_home_bordering.png
+  static const String assetsImagesBackgroundHomeBordering =
+      "assets/images/Background_home_bordering.png";
+
+  /// Assets for assetsImagesDoctorHome
+  /// assets/images/doctor_home.png
+  static const String assetsImagesDoctorHome = "assets/images/doctor_home.png";
+
+  /// Assets for assetsImagesIconsBottomNavBarCalendar2
+  /// assets/images/icons/bottom_nav_bar/calendar-2.svg
+  static const String assetsImagesIconsBottomNavBarCalendar2 =
+      "assets/images/icons/bottom_nav_bar/calendar-2.svg";
+
+  /// Assets for assetsImagesIconsBottomNavBarHome
+  /// assets/images/icons/bottom_nav_bar/home.svg
+  static const String assetsImagesIconsBottomNavBarHome =
+      "assets/images/icons/bottom_nav_bar/home.svg";
+
+  /// Assets for assetsImagesIconsBottomNavBarMessageText
+  /// assets/images/icons/bottom_nav_bar/message-text.svg
+  static const String assetsImagesIconsBottomNavBarMessageText =
+      "assets/images/icons/bottom_nav_bar/message-text.svg";
+
+  /// Assets for assetsImagesIconsBottomNavBarSearchNormal
+  /// assets/images/icons/bottom_nav_bar/search-normal.svg
+  static const String assetsImagesIconsBottomNavBarSearchNormal =
+      "assets/images/icons/bottom_nav_bar/search-normal.svg";
+
   /// Assets for assetsImagesIconsFacebook
   /// assets/images/icons/facebook.png
   static const String assetsImagesIconsFacebook =
       "assets/images/icons/facebook.png";
 
+  /// Assets for assetsImagesIconsGeneralCategoryIcon
+  /// assets/images/icons/general_category_icon.png
+  static const String assetsImagesIconsGeneralCategoryIcon =
+      "assets/images/icons/general_category_icon.png";
+
   /// Assets for assetsImagesIconsGoogel
   /// assets/images/icons/googel.png
   static const String assetsImagesIconsGoogel =
       "assets/images/icons/googel.png";
+
+  /// Assets for assetsImagesIconsSearchNormal
+  /// assets/images/icons/search-normal.png
+  static const String assetsImagesIconsSearchNormal =
+      "assets/images/icons/search-normal.png";
+
+  /// Assets for assetsImagesIconsSpecialityCardiology
+  /// assets/images/icons/speciality/cardiology.png
+  static const String assetsImagesIconsSpecialityCardiology =
+      "assets/images/icons/speciality/cardiology.png";
+
+  /// Assets for assetsImagesIconsSpecialityDermatology
+  /// assets/images/icons/speciality/dermatology.png
+  static const String assetsImagesIconsSpecialityDermatology =
+      "assets/images/icons/speciality/dermatology.png";
+
+  /// Assets for assetsImagesIconsSpecialityGynecology
+  /// assets/images/icons/speciality/gynecology.png
+  static const String assetsImagesIconsSpecialityGynecology =
+      "assets/images/icons/speciality/gynecology.png";
+
+  /// Assets for assetsImagesIconsSpecialityNeurology
+  /// assets/images/icons/speciality/neurology.png
+  static const String assetsImagesIconsSpecialityNeurology =
+      "assets/images/icons/speciality/neurology.png";
+
+  /// Assets for assetsImagesIconsSpecialityOphthalmology
+  /// assets/images/icons/speciality/ophthalmology.png
+  static const String assetsImagesIconsSpecialityOphthalmology =
+      "assets/images/icons/speciality/ophthalmology.png";
+
+  /// Assets for assetsImagesIconsSpecialityPediatric
+  /// assets/images/icons/speciality/pediatric.png
+  static const String assetsImagesIconsSpecialityPediatric =
+      "assets/images/icons/speciality/pediatric.png";
+
+  /// Assets for assetsImagesIconsSpecialityPsychiatry
+  /// assets/images/icons/speciality/psychiatry.png
+  static const String assetsImagesIconsSpecialityPsychiatry =
+      "assets/images/icons/speciality/psychiatry.png";
 
   /// Assets for assetsImagesOnBordingBackground
   /// assets/images/on_bording_background.svg
@@ -30,38 +104,4 @@ class Assets {
   /// assets/images/on_bording_doctor.png
   static const String assetsImagesOnBordingDoctor =
       "assets/images/on_bording_doctor.png";
-
-  /// Assets for assetsImagesOnBordingPatient
-  /// assets/images/on_bording_patient.png
-  static const String assetsImagesOnBordingPatient =
-      "assets/images/Background_home_bordering.png";
-
-  /// Assets For Doctor Image Home
-  /// assets/images/doctor_home.png
-  static const String assetsImagesDoctorHome = "assets/images/doctor_home.png";
-
-  /// Asset For dermatology
-  /// assets/images/icons/speciality/dermatology.png
-  static const String assetsImagesIconsSpecialityDermatology =
-      "assets/images/icons/speciality/dermatology.png";
-
-  /// Asset for neurology
-  /// assets/images/icons/speciality/neurology.png
-  static const String assetsImagesIconsSpecialityNeurology =
-      "assets/images/icons/speciality/neurology.png";
-
-  /// Asset for psychiatry
-  /// assets/images/icons/speciality/psychiatry.png
-  static const String assetsImagesIconsSpecialityPsychiatry =
-      "assets/images/icons/speciality/psychiatry.png";
-
-  /// Asset for pediatric
-  /// assets/images/icons/speciality/pediatric.png
-  static const String assetsImagesIconsSpecialityPediatrics =
-      "assets/images/icons/speciality/pediatric.png";
-
-  // Asset for App Serach Icon
-  /// assets/images/icons/search.png
-  static const String assetsImagesIconsSearch =
-      "assets/images/icons/search-normal.png";
 }
